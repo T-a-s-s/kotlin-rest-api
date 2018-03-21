bde467e0582526fd85f73fe749e1e9f51c4cb70c
T-a-s-s <tomas.liveid@hotmail.com>
2018-03-21 14:48:58 -0300

    kotlin rest api

---
+++ src/main/kotlin/br/com/tassinfo/controllers/Greeting.kt
@@ -0,0 +1,18 @@
+package br.com.tassinfo.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+data class Greeting (val id: Long, val content: String)
+
+@RestController
+class GreetingController
+{
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "world") name: String) =
+            Greeting(counter.incrementAndGet(), "hello, $name")
+}
